a9c844a6c7abac1cb72290ffcb4f57b83f8b20e7
Gabreu <gabrielmunizgmc117@gmail.com>
2023-08-16 09:27:18 -0300

    1608

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "app/index.html")
+
+def sobre(request):
+    return render(request, "app/sobre.html")
+
+def equipe(request):
+    return render(request, "app/equipe.html")
+
+
 # Create your views here.
